6f599fcab1dfeee99e3f6be6b93ee07aa49d3eb3
David Haralambiev <114248239+CTSXT@users.noreply.github.com>
2022-12-11 21:24:46 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,8 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
+  const button = document.querySelector(".image");
   button.addEventListener("click", () => {
-    alert("💣");
+    button.style.transform = "scale(2)";
   });
 });
